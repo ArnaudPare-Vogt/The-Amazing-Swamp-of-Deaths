eef80eb4bd001bb049ae14b7523490b892cb3e7a
Arnaud <arnaud.parevogt@gmail.com>
2017-03-05 14:55:57 -0500

    Test of a new project

---
+++ game/src/tasod/core/Main.java
@@ -0,0 +1,14 @@
+package tasod.core;
+
+/**
+ * Main class of the project
+ * 
+ * @author Arnaud Paré-Vogt
+ */
+public class Main {
+	public static void main(String[] args){
+		System.out.println("allo");
+		
+		
+	}
+}
